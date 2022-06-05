ff2403780f4a4aff22efd433ec1be20ad40f442c
Diogo <diogoluxa.dev@gmail.com>
2022-06-05 15:43:53 -0300

    sidebar updated

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -13,47 +13,25 @@ import {
 } from '../../styles/Sidebar/Sidebar.styled';
 
 const Sidebar = () => {
-  const { isOpen, setIsOpen } = useContext(StateContext);
+  const { navItems, isOpen, setIsOpen } = useContext(StateContext);
 
   return (
     <SidebarStyled isOpen={isOpen}>
       <SidebarContainer>
         <SidebarMenu>
-          <SidebarItem>
-            <SidebarLink to="home" onClick={() => setIsOpen((prev) => !prev)}>
-              Home
-            </SidebarLink>
-          </SidebarItem>
-          <SidebarItem>
-            <SidebarLink
-              to="sobre"
-              className="active"
-              onClick={() => setIsOpen((prev) => !prev)}
-            >
-              Sobre
-            </SidebarLink>
-          </SidebarItem>
-          <SidebarItem>
-            <SidebarLink
-              to="atuacao"
-              onClick={() => setIsOpen((prev) => !prev)}
-            >
-              Atuação
-            </SidebarLink>
-          </SidebarItem>
-          <SidebarItem>
-            <SidebarLink to="cases" onClick={() => setIsOpen((prev) => !prev)}>
-              Cases
-            </SidebarLink>
-          </SidebarItem>
-          <SidebarItem>
-            <SidebarLink
-              to="contato"
-              onClick={() => setIsOpen((prev) => !prev)}
-            >
-              Contato
-            </SidebarLink>
-          </SidebarItem>
+          {navItems.map((item) => {
+            return (
+              <SidebarItem key={item.id}>
+                <SidebarLink
+                  to={item.title}
+                  smooth={true}
+                  onClick={() => setIsOpen((prev) => !prev)}
+                >
+                  {item.title}
+                </SidebarLink>
+              </SidebarItem>
+            );
+          })}
         </SidebarMenu>
       </SidebarContainer>
     </SidebarStyled>
